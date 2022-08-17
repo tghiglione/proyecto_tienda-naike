d37a8707dbc8a639437f6bd4147f59a5a614f672
tghiglione <tomasghiglione@gmail.com>
2022-08-17 17:05:53 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import BarraNavegacion from './components/NavBar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hola mundo!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <BarraNavegacion></BarraNavegacion>
       </header>
     </div>
   );
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../images/BrandLogo.png';
+import './Navbar.css'
+
+function BarraNavegacion() {
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="light">
+      <Container className="barraNav">
+        <Navbar.Brand href="#home"><img className='logo' src={logo} alt='logo naike'/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="barraNav_links">
+          <Nav className="barraNav_links">
+            <Nav.Link className="barraNav_item item_tienda" href="#tienda">Tienda</Nav.Link>
+            <Nav.Link className="barraNav_item" href="#calzado">Calzado</Nav.Link>
+            <Nav.Link className="barraNav_item" href="#buzos">Buzos</Nav.Link>
+            <Nav.Link className="barraNav_item" href="#remeras">Remeras</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BarraNavegacion;
+
