b7daec89ce35db7a34cf38dc8e48c7959d232909
tghiglione <tomasghiglione@gmail.com>
2022-08-24 10:39:50 -0300

    contador de productos

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import BarraNavegacion from './components/NavBar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+import { useState } from 'react';
 
 
 function App() {
+  const [numProductos,setNumProd]=useState(0);
+
+  const agregar=(contador)=>{
+    setNumProd(contador);
+    alert(`Agregado/s correctamente ${contador} producto/s al carrito`);
+  };
+
   return (
     <div className="App">
       <header className="header">
         <BarraNavegacion/>
       </header>
       <ItemListContainer titulo='NAIKE'/>
+      <ItemCount stock={8} inicial={1} agregarProducto={agregar}/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import './ItemCount.css';
+
+const ItemCount =(props)=>{
+    const [contador,setContador]=useState(props.inicial);
+    
+    const incrementar=()=>{
+        if(contador<props.stock){
+            setContador(contador+1);
+        }else{
+            alert("No hay mas stock disponible");
+        }
+        
+    };
+
+    const decrementar=()=>{
+        if(contador>0){
+            setContador(contador-1);
+        }else{
+            alert("No se puede menos de cero")
+        }
+        
+    };
+
+    return(
+        <>
+            <div className="contador">
+                <button onClick={decrementar} className="btn-contador">-</button>
+                <h2>{contador}</h2>
+                <button onClick={incrementar} className="btn-contador">+</button>    
+            </div>
+            <div>
+                <button onClick={()=>props.agregarProducto(contador)} className="btn-carrito">Agregar al carrito</button>
+            </div>
+        </>
+       
+    )
+};
+
+export default ItemCount;
